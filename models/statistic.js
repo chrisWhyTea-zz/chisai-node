0487bc33675648c5373e5e8c53a95fa28e5e1316
Christopher Schmitt <cs@konekobox.de>
2015-08-02 19:24:16 +0200

    adjustments to the statistic model

--- models/statistic.js
+++ models/statistic.js
@@ -1,11 +1,12 @@
 var thinky = require(__dirname+'/../util/thinky.js')
     , type = thinky.type
+    , r = thinky.r
     , shortid = require('shortid');
 
 var Statistic = thinky.createModel("Statistic", {
     id: type.string().validator(shortid.isValid).default(shortid.generate()),
     shortId: type.string(),
-    visitedAt: type.date()
+    visitedAt: type.date(r.now())
 });
 
 module.exports = Statistic;
