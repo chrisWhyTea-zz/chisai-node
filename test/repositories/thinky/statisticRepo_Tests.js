f837779725700807a4496083b5ae2d26f068ec04
Christopher Schmitt <cs@konekobox.de>
2015-08-03 07:29:37 +0200

    uuuh ... uncommited statisticRepo_Test ... =/ fixed that

--- test/repositories/thinky/statisticRepo_Tests.js
+++ test/repositories/thinky/statisticRepo_Tests.js
@@ -3,18 +3,18 @@ var chai = require('chai')
     , thinky = require(__dirname + '/../../../util/thinky')
     , r = thinky.r
     , _ = require('lodash')
-    , repo = require(__dirname + '/../../../libs/repositories/thinky/shortUrlRepo')()
+    , repo = require(__dirname + '/../../../libs/repositories/thinky/statisticRepo')()
     , models = require(__dirname + '/../../../models/all')
     , fs = require('fs')
     , async = require('async')
     , shortid = require('shortid');
 
-describe('ShortUrl Repository Tests', function () {
+describe('Statistic Repository Tests', function () {
 
-    describe('createNew() Function', function () {
+    describe('addStatistic() Function', function () {
 
-        afterEach(function (done) {
-            models.ShortUrl.run(function (err, doc) {
+        beforeEach(function (done) {
+            models.Statistic.run(function (err, doc) {
                 if (err) {
                     console.log(err);
                 } else {
@@ -29,14 +29,32 @@ describe('ShortUrl Repository Tests', function () {
             });
         });
 
-        it('create a new shortUrl', function (done) {
-            repo.createNew('http://konekobox.de', function (err, entry) {
+        afterEach(function (done) {
+            models.Statistic.run(function (err, doc) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    async.each(doc, function (entry, callback) {
+                        entry.deleteAll(null, callback);
+                    }, function (err) {
+                        if (!err) {
+                            done();
+                        }
+                    });
+                }
+            });
+        });
+
+        it('create a new Statistic Entry', function (done) {
+            var fakeId = shortid.generate();
+            repo.addStatistic(fakeId, function (err, entry) {
                 expect(err).to.be.null;
-                expect(entry).not.to.be.null;
-                expect(entry.target).to.be.equal('http://konekobox.de');
-                models.ShortUrl.get(entry.id)
-                    .then(function (shorturlEntry) {
-                        expect(shorturlEntry.target).to.be.equal('http://konekobox.de');
+                //expect(entry).not.to.be.null;
+                //expect(entry.target).to.be.equal('http://konekobox.de');
+                models.Statistic
+                    .then(function (statsEntrys) {
+                        expect(statsEntrys).to.have.length(1);
+                        expect(_.first(statsEntrys).shortId).to.be.equal(fakeId);
                         done();
                     })
                     .error(function (err) {
@@ -44,116 +62,6 @@ describe('ShortUrl Repository Tests', function () {
                     });
             });
         });
-
-        it('does not create a new shortUrl | provided URL is a invalid URL', function (done) {
-            repo.createNew('http://konekoboxde', function (err, entry) {
-                expect(err).not.to.be.null;
-                expect(err.message).to.be.equal("ValidationInvalidUrl");
-                expect(entry).to.be.null;
-                done();
-            });
-        });
     });
 
-    describe('getTargetById() Function', function () {
-        var testShortUrlInDB;
-
-        beforeEach(function (done) {
-            var testShortUrl = new models.ShortUrl(
-                {
-                    target: 'http://google.com',
-                    id: shortid.generate(),
-                    createdAt: r.now()
-                });
-            testShortUrl.save().then(function (shortUrl) {
-                testShortUrlInDB = shortUrl;
-                done();
-            });
-        });
-
-        afterEach(function (done) {
-            models.ShortUrl.run(function (err, doc) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    async.each(doc, function (entry, callback) {
-                        entry.deleteAll(null, callback);
-                    }, function (err) {
-                        if (!err) {
-                            done();
-                        }
-                    });
-                }
-            });
-        });
-
-        it('get a shorturl with a existing id', function (done) {
-            repo.getTargetById(testShortUrlInDB.id, function (err, entry) {
-                expect(err).to.be.null;
-                expect(entry).not.to.be.null;
-                expect(entry).to.be.equal(testShortUrlInDB.target);
-                done();
-            });
-        });
-
-        it('get nothing with a non existing id', function (done) {
-            repo.getTargetById(testShortUrlInDB.id + 1, function (err, entry) {
-                expect(err).not.to.be.null;
-                expect(err.message).to.be.equal('ModelIdNotFound');
-                expect(entry).to.be.null;
-                done();
-            });
-        });
-    });
-
-    describe('getById() Function', function () {
-        var testShortUrlInDB;
-
-        beforeEach(function (done) {
-            var testShortUrl = new models.ShortUrl(
-                {
-                    target: 'http://google.com',
-                    id: shortid.generate(),
-                    createdAt: r.now()
-                });
-            testShortUrl.save().then(function (shortUrl) {
-                testShortUrlInDB = shortUrl;
-                done();
-            });
-        });
-
-        afterEach(function (done) {
-            models.ShortUrl.run(function (err, doc) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    async.each(doc, function (entry, callback) {
-                        entry.deleteAll(null, callback);
-                    }, function (err) {
-                        if (!err) {
-                            done();
-                        }
-                    });
-                }
-            });
-        });
-
-        it('get a shorturl with a existing id', function (done) {
-            repo.getById(testShortUrlInDB.id, function (err, entry) {
-                expect(err).to.be.null;
-                expect(entry).not.to.be.null;
-                expect(entry.target).to.be.equal(testShortUrlInDB.target);
-                done();
-            });
-        });
-
-        it('get nothing with a non existing id', function (done) {
-            repo.getById(testShortUrlInDB.id + 1, function (err, entry) {
-                expect(err).not.to.be.null;
-                expect(err.message).to.be.equal('ModelIdNotFound');
-                expect(entry).to.be.null;
-                done();
-            });
-        });
-    });
 });
